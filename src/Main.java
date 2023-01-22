6f709841fccbb49022007233578c51048533b27c
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2023-01-22 07:35:52 +0100

    mogollón de cambios

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,15 @@ import Empleados.Medico;
 import Habitacion.Habitacion;
 import Habitacion.ListaDeHabitaciones;
 import Paciente.Paciente;
+import RecepcionUrgencias.RecepcionUrgencias;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static String commandNotFound = "Comando erróneo";
 
     //Función para encontrar la primera habitación que se encuentre sucia
-    public Habitacion habSucia(){
+    public static Habitacion habSucia(){
         for (int i = 0; i <= ListaDeHabitaciones.habitaciones.size(); i++ ){
 
             Habitacion habitacion = ListaDeHabitaciones.habitaciones.get(i);
@@ -26,30 +29,112 @@ public class Main {
 
     //Función con las acciones del empleado médico
     public static void meds(int respuesta, Medico med){
+        //Si elige pasar consulta, busca la primera consulta que necesite un médico, y pasa consulta
         if(respuesta==1){
             for(Consulta consulta:ListaDeConsultas.consultas){
                 if(consulta.getPacienteAsignado()!= null){
                     med.VisitaConsultas(consulta.getPacienteAsignado());
+                }}}
+        //Si elige visitar habitación, busca la primera habitación que necesite un médico
+        else if(respuesta==2){
+            for(Habitacion habitacion:ListaDeHabitaciones.habitaciones){
+                if(habitacion.getPacienteAsignado()!= null){
+                    med.MandarTratamientos(habitacion.getPacienteAsignado(),"tratamiento");
+                    habitacion.getPacienteAsignado().setHabitacion(null);
+                    habitacion.setPacienteAsignado(null);
+                    habitacion.setOcupacion(false);
+                }}
                 }
-                else if(respuesta==2){
-
-                }
-                else System.out.println("Comando erróneo.");
+        else System.out.println(commandNotFound);
             }
-        }
-    }
+
 
     //Función con las acciones del empleado enfermero
-    public static void enfs(int respuesta, Enfermero enf){}
+    public static void enfs(int respuesta, Enfermero enf){
+        if(respuesta==1){
+            for(Consulta consulta:ListaDeConsultas.consultas){
+                if(consulta.getPacienteAsignado()!= null){
+                    System.out.println(enf.getNombre()+"atiende a "+consulta.getPacienteAsignado().getNombre()+" en la consulta "+
+                            consulta.getNumero());
+                }}}
+        else if(respuesta==2){
+            for(Habitacion habitacion:ListaDeHabitaciones.habitaciones){
+                if(habitacion.getPacienteAsignado()!= null){
+                    System.out.println(enf.getNombre()+"atiende a "+habitacion.getPacienteAsignado().getNombre()+" en la habitación "+
+                            habitacion.getNumero());
+                }}
+        }
+        else System.out.println(commandNotFound);
+    }
 
     //Función con las acciones del empleado limpiador
-    public static void limps(int respuesta, Limpiador limp){}
+    public static void limps(int respuesta, Limpiador limp){
+        if(respuesta==1){
+            Habitacion hab = habSucia();
+            limp.setHab(hab);
+            System.out.println("Se te asigna la habitación "+hab.getNumero());
+        }
+        //Si elige limpiar una habitación, en caso de que ya tuviera una asignada limpia esa, pero sino tenía ninguna
+        //busca la primera habitación que necesite ser limpiada
+        else if(respuesta==2){
+            if(limp.getHab()!=null){
+            limp.Limpiar(limp.getHab());}
+            else{
+                limp.Limpiar(habSucia());
+            }
+        }
+        else System.out.println(commandNotFound);
+    }
 
     //Función con las acciones del empleado celador
-    public static void cels(int respuesta, Celador cel){}
+    public static void cels(int respuesta, Celador cel){
+        Paciente paciente = null;
+        for(Habitacion habitacion:ListaDeHabitaciones.habitaciones){
+            if(habitacion.getOcupacion()==true){
+                paciente = habitacion.getPacienteAsignado();
+            }
+        }
+        switch(respuesta){
+            case 1:
+                cel.TransportarPacientes(paciente,paciente.getHabitacion());
+                System.out.println("Transporta al paciente "+paciente.getNombre()+" a la habitación "+paciente.getHabitacion());
+                break;
+            case 2:
+                cel.LLevarComida(paciente,"comida");
+                System.out.println("Lleva comida a la habitación "+paciente.getHabitacion());
+                break;
+            case 3:
+                cel.HacerCuras(paciente);
+                System.out.println("Realiza curas al paciente "+paciente.getNombre()+" de la habitación "+paciente.getHabitacion());
+                break;
+            default:
+                System.out.println(commandNotFound);
+                break;}
+    }
 
     //Función con las acciones del paciente
-    public static void pacs(){}
+    public static void pacs(){
+        //Pide los datos al paciente
+        Scanner dLines = new Scanner(System.in);
+        System.out.println("Introduce tu dni: ");
+        String dni = dLines.nextLine();
+        System.out.println("Introduce tu nombre: ");
+        String nombre = dLines.nextLine();
+        System.out.println("Introduce tus apellidos: ");
+        String apellidos = dLines.nextLine();
+        System.out.println("Introduce tu número de registro: ");
+        String num = dLines.nextLine();
+        System.out.println("¿Qué te ocurre?");
+        String dolencia = dLines.nextLine();
+        Paciente pac = new Paciente(dni,nombre,apellidos,num,dolencia);
+        //Se le da un número random para la gravedad para simular el paso por la revisión en urgencias
+        Random rand = new Random();
+        int x = rand.nextInt(1)+1;
+        pac.setGravedad(x);
+        //En base a su gravedad se decide qué hacer con el paciente
+        RecepcionUrgencias urg = new RecepcionUrgencias();
+        urg.asignacion(pac);
+    }
     public static void main() {
         //Datos de prueba
         Medico[] medicos = new Medico[]{new Medico("6438999I","Antonio","Otorrino"),
@@ -143,6 +228,7 @@ public class Main {
                         break;
                     default:
                         System.out.println("Número erróneo.");
+                        break;
                 }
             }
             else if(respuesta==2){
